8f80547fc4178a5370ac85508ed1aa7642dc5db7
Izan Beltrán <izanbf1803@gmail.com>
2019-05-04 22:34:53 +0200

    Backup

--- 15 - Seat Planner/main.cc
+++ 15 - Seat Planner/main.cc
@@ -2,14 +2,159 @@
 using namespace std;
 #define D(x) cout << #x << " = " << x << ", "
 
+int n; // number of people
+vector<vector<int>> adj;
+vector<int> parent;
+vector<int> size;
+vector<int> unions;
+vector<vector<int>> dp;
+vector<bool> visited;
+
+int ufind(int u)
+{
+    if (parent[u] == -1) return u;
+    parent[u] = ufind(parent[u]);
+    return parent[u];
+}
+
+void ujoin(int u, int v)
+{
+    u = ufind(u);
+    v = ufind(v);
+    if (u != v) {
+        parent[v] = u;
+        size[u] += size[v];
+    }
+}
+
+int unions_subset_with_size(int m, int s)
+{
+    // m: unions subarray size, s: objective size
+    int& ans = dp[m][s];
+    if (ans != -1) return ans;
+    if (s == 0) {
+        ans = 1;
+    }
+    else if (m == 0) {
+        ans = 0;
+    }
+    else if (size[unions[m-1]] > s) {
+        ans = unions_subset_with_size(m-1, s);
+    }
+    else {
+        ans = unions_subset_with_size(m-1, s) | unions_subset_with_size(m-1, s-size[unions[m-1]]);
+    }
+    return ans;
+}
+
+vector<int> reconstruct_subset_with_size(int m, int s)
+{
+    if (s == 0) {
+        return {};
+    }
+    else if (m == 0) {
+        return {-1};
+    }
+    else if (size[unions[m-1]] > s) {
+        return reconstruct_subset_with_size(m-1, s);
+    }
+    else {
+        if (unions_subset_with_size(m-1, s)) {
+            return reconstruct_subset_with_size(m-1, s);
+        }
+        else if (unions_subset_with_size(m-1, s-size[unions[m-1]])) {
+            vector<int> sol = reconstruct_subset_with_size(m-1, s-size[unions[m-1]]);
+            sol.push_back(unions[m-1]);
+            return sol;
+        }
+        return {-1}; // error
+    }
+}
+
+int find_dfs_root(int u)
+{
+    for (int i = 1; i <= n; ++i) {
+        if (ufind(i) == u and adj[i].size() == 1) {
+            return i;
+        }
+    }   
+    return u; // if not dfs root found
+}
+
+void solve_union(vector<int>& v, int i)
+{
+    visited[i] = true;
+    for (int x : adj[i]) {
+        if (not visited[x]) solve_union(v, x);
+    }
+    v.push_back(i);
+}
+
 int main(int argc, char** argv)
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    if (argc != 2) {
+        cout << "ERROR: INVALID INPUT" << endl;
+        exit(1);
+    }
     istringstream in(argv[1]);
     int S, K;
     in >> S >> K;
+    n = 8*S;
+    adj.resize(n+1);
+    parent.assign(n+1, -1);
+    size.assign(n+1, 1);
+
     for (int i = 0; i < K; ++i) {
         int a, b;
         in >> a >> b;
+        if (a > b) swap(a, b);
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+        ujoin(a, b);
     }
-    cout << endl;
+
+    for (int i = 1; i <= n; ++i) {
+        if (ufind(i) == i) {
+            unions.push_back(i);
+        }
+    }
+
+    sort(unions.begin(), unions.end(), [](int u, int v){
+        return size[u] > size[v];
+    });
+
+    vector<vector<int>> tables(8);
+
+    for (int t = 0; t < 8; ++t) {
+        dp.assign(unions.size()+1, vector<int>(S+1, -1));
+        vector<int> sol = reconstruct_subset_with_size(unions.size(), S);
+        for (int u : sol) {
+            tables[t].push_back(u);
+            unions.erase(find(unions.begin(), unions.end(), u));
+        }
+    }
+
+    for (int t = 0; t < 8; ++t) {
+        vector<int> table;
+        for (int u : tables[t]) {
+            visited.assign(n+1, false);
+            vector<int> sol;
+            solve_union(sol, find_dfs_root(u));
+            for (int x : sol) {
+                table.push_back(x);
+            }
+        }
+        tables[t] = table;
+    }
+
+    for (int t = 0; t < 8; ++t) {
+        for (int i = 0; i < S; ++i) {
+            if (i > 0) cout << ",";
+            cout << tables[t][i];
+        }
+        cout << endl;
+    }
 }
--- 15 - Seat Planner/main.py
+++ 15 - Seat Planner/main.py
@@ -1,35 +1,51 @@
-import socket, sys, subprocess
+import socket, sys, subprocess, time
+
+DEBUG_SOCK = False
 
 def sock_send(s, data):
     s.send(data.encode())
-    print(f"SEND:\n---\n{data}---\n")
+    if DEBUG_SOCK:
+        print(f"SEND:\n---\n{data}---\n")
 
 def sock_read(s):
-    data = s.recv(1024).decode()
-    print(f"RECV:\n---\n{data}---\n")
+    data = s.recv(1048576).decode()
+    if DEBUG_SOCK:
+        print(f"RECV:\n---\n{data}---\n")
     return data
 
 def solve_test(s, test):
+    global t0, solver_input, sol, lines
     try:
         lines = list(filter(lambda x: len(x) != 0, sock_read(s).split('\n')))
         if lines[0].find("Validator") != -1:
             raise Exception(lines[0])
-        result = subprocess.run(["solver.exe", ' '.join(lines)], stdout=subprocess.PIPE)
-        print(f"solver.exe RESULT:\n---\n{result.stdout.decode()}---\n")
+        solver_input = ' '.join(lines)
+        result = subprocess.run(["solver.exe", solver_input], stdout=subprocess.PIPE)
+        # print("SOLVER_INPUT:", solver_input)
+        sol = result.stdout.decode()
+        # print(f"solver.exe RESULT:\n---\n{sol}---\n")
         # TODO: call C++ solver
-        for i in range(0, 8):
-            sock_send(s, ','.join(str(8*i+j) for j in range(1, 9))+"\n")
+        for line in sol.splitlines():
+            sock_send(s, line+"\n")
+        # for i in range(0, 8):
+        #     sock_send(s, ','.join(str(8*i+j) for j in range(1, 9))+"\n")
     except Exception as e:
         print(f"ERROR on test #{test-1} => {e}")
+        print("LINES:", lines)
+        print("SOLVER_INPUT:", solver_input)
+        print(f"solver.exe RESULT:\n---\n{sol}---\n")
+        print(time.time()-t0)
         sys.exit(1)
 
 subprocess.run(["make"])
 IP = "52.49.91.111"
 PORT = 1888
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(1)
+s.settimeout(5)
 s.connect((IP, PORT))
-s.send(b"TEST\n")
+s.send(b"SUBMIT\n")
+t0 = time.time()
 N = int(sock_read(s))
 for test in range(1,N+1):
     solve_test(s, test)
+print(sock_read(s))
